e492a451e019e58f4405d33f7bde82324c30d576
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-10-04 00:52:45 +0600

    Module 51 practice

--- src/component/PhoneBar/PhoneBar.js
+++ src/component/PhoneBar/PhoneBar.js
@@ -1,14 +1,35 @@
+import { data } from 'autoprefixer';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const PhoneBar = () => {
     const [phones, setPhones] = useState([])
     useEffect(() => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-            .then(data => console.log(data))
+            .then(data => {
+                const phoneLoader = data.data.data
+                const phoneData = phoneLoader.map(phone => {
+                    const parts = phone.slug.split('-')
+                    const price = parseInt(parts[1])
+                    const singlePhone = {
+                        name: phone.phone_name,
+                        price: price
+                    }
+                    return singlePhone
+                })
+                console.log(phoneData)
+                setPhones(phoneData)
+            })
     }, {})
     return (
-        <div>
+        <div className='flex items-center justify-center my-12'>
+            <BarChart width={600} height={200} data={phones}>
+                <Bar dataKey='price' fill='red'></Bar>
+                <XAxis dataKey='name'></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
 
         </div>
     );
